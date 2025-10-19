0ccf38c6f64bf956c1c02d8f7bdcc65f8892d13f
Carly E Karrick <157175206+ckarrick@users.noreply.github.com>
2025-10-19 06:31:31 -0700

    Delete download_data.sh

--- download_data.sh
+++
@@ -1,7 +0,0 @@
-# For each sample's accession number
-for sra in SRR1708168 SRR1708296; do
-  # Download the SRA file
-  ../programs/sratoolkit.3.2.1-alma_linux64/bin/prefetch $sra
-  # Convert the SRA file to a fastq file
-  ../programs/sratoolkit.3.2.1-alma_linux64/bin/fasterq-dump $sra
-done
