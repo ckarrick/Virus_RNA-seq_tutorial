7b658c83fa3b446f0196f8f2ced73b7d3d6bb4db
Carly E Karrick <157175206+ckarrick@users.noreply.github.com>
2025-10-20 22:22:26 -0700

    Update download_data.sh

--- data/download_data.sh
+++ data/download_data.sh
@@ -3,7 +3,7 @@
 # This script downloads raw RNA-Seq data for the virus RNA-Seq tutorial
 
 # For each sample's accession number
-for sra in SRR1708168 SRR1708296; do
+for sra in SRR30671252 SRR30671260; do
   # Download the SRA file
   ../programs/sratoolkit.3.2.1-alma_linux64/bin/prefetch $sra
   # Convert the SRA file to a fastq file
