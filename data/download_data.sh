0b283b127b90428e3bef0cbffb643e315a5e7d72
Carly E Karrick <157175206+ckarrick@users.noreply.github.com>
2025-10-21 12:54:23 -0700

    Update download_data.sh

--- data/download_data.sh
+++ data/download_data.sh
@@ -6,6 +6,6 @@
 for sra in SRR30671252 SRR30671260; do
   # Download the SRA file
   ../programs/sratoolkit.3.2.1-alma_linux64/bin/prefetch $sra
-  # Convert the SRA file to a fastq file
+  # Convert the SRA file to a FASTQ file
   ../programs/sratoolkit.3.2.1-alma_linux64/bin/fasterq-dump $sra
 done
